4a723c45f3a1850af27275d29c65a8e1272c7655
FranciscoSilv4a <franciscofelipedasilva457@gmail.com>
2025-11-01 21:50:54 -0300

    mudanças pequenas

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
+<?php
+    //
+?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Menu</title>
 </head>
 <body>
-    
+    <div class="mainContainer">
+        <div class="secundaryCont">
+            <p class="title"></p>
+            <div class="contUser">
+                <button class="btn" id="btnLogin">Logar</button>
+                <button class="btn" id="btnCadastro">Cadastrar</button>
+            </div>
+            <button class="btn">Jogar</button>
+            <button class="btn">Placar</button>
+            <button class="btn">Créditos</button>
+        </div>
+    </div>
 </body>
 </html>
