48cbea5a3f3318af44d0c9d17933391d4b663b24
FranciscoSilv4a <franciscofelipedasilva457@gmail.com>
2025-11-01 22:53:45 -0300

    botoes iniciais

--- index.php
+++ index.php
@@ -8,18 +8,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <link rel="stylesheet" href="style/index.css">
+    <script defer src="js/index.js"></script>
 </head>
 <body>
     <div class="mainContainer">
         <div class="secundaryCont">
-            <p class="title"></p>
+            <p class="title">Don't Look at Me</p>
             <div class="contUser">
                 <button class="btn" id="btnLogin">Logar</button>
                 <button class="btn" id="btnCadastro">Cadastrar</button>
             </div>
-            <button class="btn">Jogar</button>
-            <button class="btn">Placar</button>
-            <button class="btn">Créditos</button>
+            <button class="btn" id="btnJogar">Jogar</button>
+            <button class="btn" id="btnPlacar">Placar</button>
+            <button class="btn" id="btnCreditos">Créditos</button>
         </div>
     </div>
 </body>
