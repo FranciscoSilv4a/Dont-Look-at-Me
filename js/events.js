fb38a7a4d7097d6c25a9742fbae25a6e27eb8d9e
FranciscoSilv4a <franciscofelipedasilva457@gmail.com>
2025-11-04 19:47:14 -0300

    primeira fase

---
+++ js/events.js
@@ -0,0 +1,19 @@
+// animações da mão
+function pointerHand() {
+    let hand = document.querySelector('.hand');
+    hand.style.backgroundImage = "url('issets/handPointer.png')";
+}
+function normalHand() {
+    let hand = document.querySelector('.hand');
+    hand.style.backgroundImage = "url('issets/handIcon.png')";
+}
+
+// abrir e fechar a aba de dicas
+function openDicas() {
+    let dicaContainer = document.querySelector(".dicaContainer");
+    dicaContainer.style.display = "flex";
+}
+function closeDicas() {
+    let dicaContainer = document.querySelector(".dicaContainer");
+    dicaContainer.style.display = "none";
+}
--- lvl1.php
+++ lvl1.php
@@ -9,19 +9,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nível 1</title>
     <link rel="stylesheet" href="style/lvl1.css">
-    <script defer src="js/index.js"></script>
+    <script defer src="js/lvl1.js.js"></script>
+    <script defer src="js/events.js"></script>
 </head>
 <body>
     <div class="mainContainer">
-        <div class="title"></div>
-        <div class="contUser">
-            <button class="btn" id="btnLogin">Logar</button>
-            <button class="btn" id="btnCadastro">Cadastrar</button>
-        </div>
-        <button class="btn" id="btnJogar">Jogar</button>
-        <button class="btn" id="btnPlacar">Placar</button>
-        <button class="btn" id="btnCreditos">Créditos</button>
+        <div class="title">Tentativa número 1</div>
+        <p class="txt">
+            Esta criatura aparenta estar calma, refletindo sobre sua própria existência. Entretanto, mantenha distância 
+            não sabemos que tipos de vírus e patógenos, mesmo as mais inofensivas anomalias, podem carregar.
+             O que está a sua frente 
+            se classifica como uma CRIATURA ANÔMALA NEUTRA.
+        </p>
     </div>
+    <div class="btnContainer">
+        <p class="title">
+            Possibilidades:
+        </p>
+        <button class="btn" id="btnA" onmouseover="pointerHand()" onmouseout="normalHand()">a) Esqueleto solitário</button>
+        <button class="btn" id="btnB" onmouseover="pointerHand()" onmouseout="normalHand()">b) Nietzsche ressucitado</button>
+        <button class="btn" id="btnC" onmouseover="pointerHand()" onmouseout="normalHand()">c) Seu eu verdadeiro</button>
+        <button class="btn" id="btnC" onmouseover="pointerHand()" onmouseout="normalHand()">d) Senhor dos mares</button>
+        <div class="dica" onmouseover="pointerHand()" onmouseout="normalHand()" onclick="openDicas()"></div>
+    </div>
+    <div class="dicaContainer">
+        <div class="close" onmouseover="pointerHand()" onmouseout="normalHand()" onclick="closeDicas()"></div>
+        <p class="title" id="titleDicas">Dicas</p>
+        <p class="txt">
+            1) A anomalia não está viva, no sentido convencional.
+        </p>
+        <p class="txt">
+            2) A criatura não é uma personificação do abstrato.
+        </p>
+        <p class="txt">
+            3) O alvo não aparenta ter nenhuma autoridade sobre a natureza.
+        </p>
+    </div>
+    <div class="imgBlack"></div>
+    <div class="inventario">
+        <div class="itemInventario" id="item1"></div>
+        <div class="itemInventario" id="item2"></div>
+        <div class="itemInventario" id="item3"></div>
+        <div class="itemInventario" id="item4"></div>
+        <div class="itemInventario" id="item5"></div>
+    </div>
+    <div class="hand"></div>
     <p class="version">Versão 0.01</p>
 </body>
 </html>
